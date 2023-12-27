3681a88a5515e0a6494f06384aa412fee5631b25
Minus0149 <minus.official.01@gmail.com>
2023-12-27 21:14:49 +0530

    Created a basic header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,36 @@
+import Link from "next/link";
+import { FaUserCircle } from "react-icons/fa";
+import { MdDarkMode } from "react-icons/md";
+
 const Header = () => {
-	return <div>Header</div>;
+	return (
+		<header>
+			<div>
+				<Link href="/">Hotelz</Link>
+				<ul>
+					<li>
+						<Link href="/auth">
+							<FaUserCircle />
+						</Link>
+					</li>
+					<li>
+						<MdDarkMode />
+					</li>
+				</ul>
+			</div>
+			<ul>
+				<li>
+					<Link href="/">Home</Link>
+				</li>
+				<li>
+					<Link href="/rooms">Rooms</Link>
+				</li>
+				<li>
+					<Link href="/">Contacts</Link>
+				</li>
+			</ul>
+		</header>
+	);
 };
 
 export default Header;
