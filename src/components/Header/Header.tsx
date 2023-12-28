4c8e091baeac23ff883aee6ec959200d5faff991
Minus0149 <minus.official.01@gmail.com>
2023-12-28 20:47:07 +0530

    Added basic styles for header using tailwind Css

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,28 +4,30 @@ import { MdDarkMode } from "react-icons/md";
 
 const Header = () => {
 	return (
-		<header>
-			<div>
-				<Link href="/">Hotelz</Link>
-				<ul>
-					<li>
+		<header className="py-10 px-4 container mx-auto text-xl flex flex-wrap md:flex-nowrap items-center justify-between">
+			<div className="flex items-center w-full md:2/3">
+				<Link href="/" className="font-black text-[#f27405e1]">
+					Hotelz
+				</Link>
+				<ul className="flex items-center ml-5">
+					<li className="flex items-center">
 						<Link href="/auth">
-							<FaUserCircle />
+							<FaUserCircle className="cursor-pointer" />
 						</Link>
 					</li>
-					<li>
-						<MdDarkMode />
+					<li className="ml-2">
+						<MdDarkMode className="cursor-pointer" />
 					</li>
 				</ul>
 			</div>
-			<ul>
-				<li>
+			<ul className="flex items-center justify-between w-full md:w-1/3">
+				<li className="hover:-translate-y-2 duration-500 transition-all hover:text-[#f27405]">
 					<Link href="/">Home</Link>
 				</li>
-				<li>
+				<li className="hover:-translate-y-2 duration-500 transition-all hover:text-[#f27405]">
 					<Link href="/rooms">Rooms</Link>
 				</li>
-				<li>
+				<li className="hover:-translate-y-2 duration-500 transition-all hover:text-[#f27405]">
 					<Link href="/">Contacts</Link>
 				</li>
 			</ul>
